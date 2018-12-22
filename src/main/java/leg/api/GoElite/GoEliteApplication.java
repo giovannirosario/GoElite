d83ad18b6e5cbece4c4315a09f7e9d3db3314c73
giovannirosario <giovanni.rosario@hotmail.com>
2018-12-21 21:13:08 -0300

    Rebase Spring Setup

---
+++ src/main/java/leg/api/GoElite/GoEliteApplication.java
@@ -0,0 +1,14 @@
+package leg.api.GoElite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoEliteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoEliteApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/leg/api/GoElite/GoEliteApplicationTests.java
@@ -0,0 +1,17 @@
+package leg.api.GoElite;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GoEliteApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
