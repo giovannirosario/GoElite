b6e1986ea8ae014f6f40891cc0a582c9dc778439
giovannirosario <giovanni.rosario@hotmail.com>
2018-12-23 23:19:47 -0300

    Added User model class

---
+++ src/main/java/leg/api/GoElite/models/User.java
@@ -0,0 +1,48 @@
+package leg.api.GoElite.models;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+
+public class User {
+	@Id
+	private ObjectId _id;
+	
+	private String name;
+	private String email;
+	private String sex;
+	private int weight;
+	private String phone;
+	private String cpf;
+	private String adress;
+	private String dob;
+	private String box;
+	
+	public User() {}
+	
+	public String getName() {return this.name;}
+	public void setName(String name) {this.name = name;}
+	
+	public String getEmail() {return this.email;}
+	public void setEmail(String email) {this.email = email;}
+	
+	public String getSex() {return this.sex;}
+	public void setSex(String sex) {this.sex = sex;}
+	
+	public int getWeight() {return this.weight;}
+	public void setWeight(int weight) {this.weight = weight;}
+	
+	public String getPhone() {return this.phone;}
+	public void setPhone(String phone) {this.phone = phone;}
+	
+	public String getCPF() {return this.cpf;}
+	public void setCPF(String cpf) {this.cpf = cpf;}
+	
+	public String getAdress() {return this.adress;}
+	public void setAdress(String adress) {this.adress = adress;}
+	
+	public String getDOB() {return this.dob;}
+	public void setDOB(String dob) {this.dob = dob;}
+	
+	public String getBox() {return this.box;}
+	public void setBox(String box) {this.box = box;}
+}
